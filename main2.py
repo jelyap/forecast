8f6a9d44f4878177093a02d7ff9db22ef7cab035
Erika Yap <33529114+jelyap@users.noreply.github.com>
2022-01-27 11:45:13 -0800

    Update main2.py

--- main2.py
+++ main2.py
@@ -10,7 +10,7 @@ st.title("Carol's Forecast App")
 def conn():
     dbname = os.getenv(st.secrets["DBNAME"])
     host = os.getenv(st.secrets["HOST"])
-    port = os.getenv(str(st.secrets["PORT"]))
+    port = os.getenv(st.secrets["PORT"])
     user = os.getenv(st.secrets["USERDB"])
     password = os.getenv(st.secrets["PASSWORD"])
     print('postgresql://' + str(user) + ':' + str(password) + '@' + str(host) + ':' + str(port) + '/' + str(dbname))
@@ -22,7 +22,7 @@ def conn():
 def dev_conn():
     dbname = os.getenv(st.secrets["DBNAME_DEV"])
     host = os.getenv(st.secrets["HOST_DEV"])
-    port = os.getenv(str(st.secrets["PORT"]))
+    port = os.getenv(st.secrets["PORT"])
     user = os.getenv(st.secrets["USERDB_DEV"])
     password = os.getenv(st.secrets["PASSWORD"])
     print('postgresql://' + str(user) + ':' + str(password) + '@' + str(host) + ':' + str(port) + '/' + str(dbname))
