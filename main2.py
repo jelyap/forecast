3c425e873ff4bdc2a7446588f2a1ea83fe9383ce
Erika Yap <33529114+jelyap@users.noreply.github.com>
2022-01-31 11:17:24 -0800

    Update main2.py

--- main2.py
+++ main2.py
@@ -194,7 +194,7 @@ if __name__ == '__main__':
                         select to_char("transaction-time", 'YYYY-MM-DD') as "transaction-time", sum(quantity) 
                         from public.custom_mview_genv cmg 
                         where "product-active" = 1
-                        and "product-sku" = '""" + strip(str(sku_choice)) + "'" + """ group by "transaction-time" """ 
+                        and "product-sku" = '""" + (str(sku_choice)) + "'" + """ group by "transaction-time" """ 
                      
 
         df2 = pd.read_sql(query_prod, engine)
