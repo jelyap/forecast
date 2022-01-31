b45050428f236e13e3df33eb88e1ae733ed3ddd9
Erika Yap <33529114+jelyap@users.noreply.github.com>
2022-01-31 11:54:09 -0800

    Update main2.py

--- main2.py
+++ main2.py
@@ -191,7 +191,7 @@ if __name__ == '__main__':
         engine = conn()
 
         query_prod = """
-                        select to_char("transaction-time", 'YYYY-MM-DD') as "transaction-time", sum(quantity) 
+                        select to_char("transaction-time", 'YYYY-MM-DD') as "transaction-time", sum(quantity) as quantity
                         from public.custom_mview_genv cmg 
                         where "product-active" = 1
                         and "product-sku" = '""" + (str(sku_choice)) + "'" + """ group by 1 order by "transaction-time" """ 
@@ -199,4 +199,4 @@ if __name__ == '__main__':
 
         df2 = pd.read_sql(query_prod, engine)
     
-        AgGrid(df2)
+        st.line_chart(df)
